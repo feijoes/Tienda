abf02042ea03292c48550813edaaecc4887908c1
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-04-20 21:57:17 -0300

    add react files

--- frotend/urls.py
+++ frotend/urls.py
@@ -1,10 +1,9 @@
 import re
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
     path('register/',form),
     path('media/images/<str:imagen>',image),
+    path('',index)
 ]
--- frotend/views.py
+++ frotend/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 from django.http import FileResponse
 # Create your views here.
 def form(request):
-    print(request)
     file = '\\'.join(__file__.split('\\')[:-1]) + '/templates/registro.html'
     return render(request, file)
+
+def index(request):
+    return render(request, 'frontend/index.html')
 
 def image(request,imagen):
     
