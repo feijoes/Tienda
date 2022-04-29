9b31b16f04c593da1bbec5691e1cf76c2c649e1f
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-04-29 20:20:34 -0300

    heroku

--- tienda/wsgi.py
+++ tienda/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+from operator import imod
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tienda.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
